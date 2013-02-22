f1b15d38548f2d2d130b9ac4696f1db9667b1fb5
Vlad Zaharia <vladzaharia@gmail.com>
2013-02-22 15:15:12 -0800

    UI Styling. Added in Bootstrap and Edited Header.

--- SasquatchCAIRS/SasquatchCAIRS/Controllers/AccountController.cs
+++ SasquatchCAIRS/SasquatchCAIRS/Controllers/AccountController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Transactions;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Security;
-using DotNetOpenAuth.AspNet;
-using Microsoft.Web.WebPages.OAuth;
-using WebMatrix.WebData;
+﻿using Microsoft.Web.WebPages.OAuth;
 using SasquatchCAIRS.Filters;
 using SasquatchCAIRS.Models;
+using System;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebMatrix.WebData;
 
 namespace SasquatchCAIRS.Controllers {
     [Authorize]
--- SasquatchCAIRS/SasquatchCAIRS/Controllers/HomeController.cs
+++ SasquatchCAIRS/SasquatchCAIRS/Controllers/HomeController.cs
@@ -1,12 +1,4 @@
-﻿using SasquatchCAIRS.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Configuration;
-using System.DirectoryServices;
-using System.Web.Hosting;
+﻿using System.Web.Mvc;
 
 namespace SasquatchCAIRS.Controllers {
     public class HomeController : Controller {
