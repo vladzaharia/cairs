d3c4e756493be59b90152447677e5957672d5f69
Vlad Zaharia <vladzaharia@gmail.com>
2013-03-11 19:51:01 -0700

    Fill in more information on view request page + Keywords

--- SasquatchCAIRS/SasquatchCAIRS/Controllers/HomeController.cs
+++ SasquatchCAIRS/SasquatchCAIRS/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using SasquatchCAIRS.Controllers.Security;
 using SasquatchCAIRS.Models;
@@ -7,8 +8,9 @@ namespace SasquatchCAIRS.Controllers {
     public class HomeController : Controller {
         [Authorize]
         public ActionResult Index() {
-            var db = new CAIRSDataContext();
+            CAIRSDataContext db = new CAIRSDataContext();
             RequestLockController rlc = RequestLockController.instance;
+            Dictionary<long, List<string>> keywords = new Dictionary<long, List<string>>();
 
             // Select all from Requests
             IQueryable<Request> requests = db.Requests.Select(r => r);
@@ -32,13 +34,34 @@ namespace SasquatchCAIRS.Controllers {
 
             requests = requests.OrderByDescending(r => r.TimeOpened).Take(20);
 
-            // TODO: Keywords
-
             // Set the requests to null if there isn't anything on it,
             // as the view doesn't seem to have Any() available.
             if (!requests.Any() || !User.IsInRole(Constants.Roles.VIEWER)) {
                 requests = null;
             }
+
+            // Grab keywords for the requests
+            if (requests != null) {
+                foreach (Request rq in requests) {
+                    foreach (Keyword keyword in rq.QuestionResponses
+                        .Select(qr => db.KeywordQuestions.Where(kq =>
+                            kq.QuestionResponseID == qr.QuestionResponseID &&
+                            kq.RequestID == qr.RequestID))
+                        .SelectMany(kqs => kqs.Select(kq => 
+                            db.Keywords.FirstOrDefault(
+                                k => k.KeywordID == kq.KeywordID))
+                        .Where(keyword => keyword != null))) {
+                        if (keywords.ContainsKey(rq.RequestID)) {
+                            keywords[rq.RequestID].Add(
+                                keyword.KeywordValue);
+                        } else {
+                            List<string> kwlist = new List<string> { keyword.KeywordValue };
+                            keywords.Add(rq.RequestID, kwlist);
+                        }
+                    }
+                }
+            }
+
             ViewBag.Requests = requests;
 
             return View();
