ccca6d005a8890525b657769dabf68a2df26dd1e
jingzhu1992 <soapbox12>
2013-03-13 18:09:05 -0700

    fixed linq query to search for keywords, qID, tID, and severity

--- SasquatchCAIRS/SasquatchCAIRS/Controllers/SearchController.cs
+++ SasquatchCAIRS/SasquatchCAIRS/Controllers/SearchController.cs
@@ -4,29 +4,28 @@ using System.Data;
 using System.Linq;
 using System.Web.Mvc;
 using SasquatchCAIRS.Controllers.ServiceSystem;
-using SasquatchCAIRS.Models;
 using SasquatchCAIRS.Models.SearchSystem;
 using SasquatchCAIRS.Models.ServiceSystem;
+using SasquatchCAIRS.Models;
 
-namespace SasquatchCAIRS.Controllers {
-    public class SearchController : Controller {
+namespace SasquatchCAIRS.Controllers
+{
+    public class SearchController : Controller
+    {
 
         private DropdownController _dropdownController =
             DropdownController.instance;
-
-        private UserProfileController _profileController =
-            new UserProfileController();
+        private SearchContext _db = new SearchContext();
 
         //
-                _profileController.getUserProfile(User.Identity.Name);
         // POST: /Search/Create
 
         [HttpPost]
         [Authorize(Roles = "Viewer")]
-        public ActionResult Search(String keywords) {
-                _profileController.getUserProfile(User.Identity.Name);
+        public ActionResult Search(String keywords)
+        {
             ViewBag.keywords = keywords;
-            var sc = new SearchCriteria();
+            SearchCriteria sc = new SearchCriteria();
             sc.keywordString = keywords;
             Session["criteria"] = sc;
             List<Request> list = searchCriteriaQuery(sc);
@@ -35,9 +34,10 @@ namespace SasquatchCAIRS.Controllers {
         }
 
         [Authorize(Roles = "Viewer")]
-        public ActionResult Advanced() {
+        public ActionResult Advanced()
+        {
 
-                _profileController.getUserProfile(User.Identity.Name);
+            SearchCriteria criteria = new SearchCriteria();
 
             ViewBag.TumorGroups =
                 _dropdownController.getActiveEntries(
@@ -49,13 +49,16 @@ namespace SasquatchCAIRS.Controllers {
         }
 
         [HttpPost]
-        [Authorize(Roles="Viewer")]
-        public ActionResult Results(SearchCriteria criteria, FormCollection form) {
+        [Authorize(Roles = "Viewer")]
+        public ActionResult Results(SearchCriteria criteria, FormCollection form)
+        {
             DateTime temp;
-            if (DateTime.TryParse(form["startTime"], out temp)) {
+            if (DateTime.TryParse(form["startTime"], out temp))
+            {
                 criteria.startTime = temp;
             }
-            if (DateTime.TryParse(form["completionTime"], out temp)) {
+            if (DateTime.TryParse(form["completionTime"], out temp))
+            {
                 criteria.completionTime = temp;
             }
             criteria.requestStatus = form["status"];
@@ -69,6 +72,7 @@ namespace SasquatchCAIRS.Controllers {
             criteria.patientLastName = form["patientLast"];
             Session["criteria"] = criteria;
 
+
             ViewBag.keywords = criteria.keywordString;
             var list = searchCriteriaQuery(criteria);
             ViewBag.ResultSetSize = list.Count;
@@ -76,38 +80,47 @@ namespace SasquatchCAIRS.Controllers {
         }
 
         [Authorize(Roles = "Viewer")]
-        public ActionResult Modify() {
-                _profileController.getUserProfile(User.Identity.Name);
+        public ActionResult Modify()
+        {
             ViewBag.TumorGroups =
                 _dropdownController.getActiveEntries(
                     Constants.DropdownTable.TumourGroup);
             ViewBag.QuestionType =
                 _dropdownController.getActiveEntries(
                     Constants.DropdownTable.QuestionType);
-            var criteria = (SearchCriteria) Session["criteria"];
+            SearchCriteria criteria = (SearchCriteria)Session["criteria"];
             return View("Advanced", criteria);
         }
 
-        protected override void Dispose(bool disposing) {
+        protected override void Dispose(bool disposing)
+        {
             _db.Dispose();
             base.Dispose(disposing);
         }
 
-        private List<string> stringToList(string input, string delimiters) {
+        private List<string> stringToList(string input, string delimiters)
+        {
             return input.Split(delimiters.ToCharArray()).ToList();
         } 
         private List<Request> searchCriteriaQuery(SearchCriteria c) {
             return (IQueryable<Request>)
                    (from r in _db.SearchResults.AsEnumerable()
-                    where r.QuestionResponses.contains(c.keywordString)
+                    
+                    //where r.QuestionResponses(c.keywordString)
                     where r.RequestStatus == c.requestStatus
-                    where r.Severity == c.severity
+                   // where r.QuestionResponses == c.severity
                     where r.PatientFName == c.patientFirstName
                     where r.PatientLName == c.patientLastName
-                    where c.tumorGroup.Contains(r.TumorGroup)
-                    where c.questionType.Contains(r.QuestionType)
+                    //where c.tumorGroup.Contains(r.TumorGroup)
+                   // where c.questionType.Contains(r.QuestionType)
                     where r.RequestorFName == c.requestorFirstName
                     where r.RequestorLName == c.requestorLastName
+                    from qr in r.QuestionResponses
+                    where qr.Severity == c.severity
+                    where qr.TumourGroupID == c.tumorGroup
+                    where qr.Severity == c.severity
+                    where qr.QuestionTypeID == c.questionType
+
                     select r).ToList();
         }
     }
