b3c18dce83be26b64cb804b3cb34147ea703b77c
Vlad Zaharia <vladzaharia@gmail.com>
2013-04-02 18:29:46 -0700

    Start of next integration test

--- SasquatchCAIRS/CAIRSTestProject/Integration/AdminDropdownTests.cs
+++ SasquatchCAIRS/CAIRSTestProject/Integration/AdminDropdownTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using SasquatchCAIRS;
 using SasquatchCAIRS.Models;
 
 namespace CAIRSTestProject.Integration {
@@ -89,6 +91,17 @@ namespace CAIRSTestProject.Integration {
 
         [Test]
         public void CreateDropDownAlreadyExists() {
+            CAIRSDataContext cdc = new CAIRSDataContext();
+            Region r =
+                cdc.Regions.FirstOrDefault(region => true);
+            Random random = new Random();
+            String codeVal = random.Next(1000000).ToString();
+            String valVal = random.Next(100000000000).ToString();
+
+            if (r == null) {
+                Assert.Fail("No Regions exist in the system!");
+            }
+
             // Navigate to the right page
             _driver.Navigate().GoToUrl(CommonTestingMethods.getURL());
             _ctm.findAndClick(Constants.UIString.ItemIDs.ADMIN,
@@ -103,6 +116,26 @@ namespace CAIRSTestProject.Integration {
             StringAssert.AreEqualIgnoringCase(
                 CommonTestingMethods.getURL() + "/Admin/Dropdown/Create/Region",
                 _driver.Url);
+
+            // Enter Already Existing Data
+            _driver.FindElement(By.Id("code")).SendKeys(r.Code);
+            _driver.FindElement(By.Id("value")).SendKeys(r.Value);
+
+            // Try to Submit the Form + Verify that we're on the same page
+            _driver.FindElement(By.Id(Constants.UIString.ItemIDs.SUBMIT_BUTTON))
+                   .Click();
+            StringAssert.AreEqualIgnoringCase(
+                CommonTestingMethods.getURL() + "/Admin/Dropdown/Create/Region",
+                _driver.Url);
+
+            // Find the Code error and check the text
+            IWebElement codeMsg =
+                _driver.FindElement(By.CssSelector("[data-valmsg-for='code']"));
+            IWebElement valMsg =
+                _driver.FindElement(By.CssSelector("[data-valmsg-for='value']"));
+
+            StringAssert.AreEqualIgnoringCase("Code cannot be empty!", codeMsg.Text);
+            StringAssert.AreEqualIgnoringCase("Value cannot be empty!", valMsg.Text);
         }
     }
 }
