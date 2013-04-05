92d47b1126ac9f9a118069550f745f7a284e6a96
Vlad Zaharia <vladzaharia@gmail.com>
2013-04-05 05:07:41 -0700

    Almost done...

--- SasquatchCAIRS/CAIRSTestProject/Integration/TestCreateRequest.cs
+++ SasquatchCAIRS/CAIRSTestProject/Integration/TestCreateRequest.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web.Security;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using SasquatchCAIRS;
+using SasquatchCAIRS.Controllers.Service;
 using SasquatchCAIRS.Models.Common;
+using SasquatchCAIRS.Models.Service;
 
 namespace CAIRSTestProject.Integration {
     [TestFixture]
@@ -155,6 +158,17 @@ namespace CAIRSTestProject.Integration {
         /// </summary>
         [Test]
         public void TestCreateRequestValid() {
+            // Create a test request in the DB
+            var rc1 = new RequestContent {
+                patientFName = "DInt-" +
+                               _random.Next()
+                                      .ToString(CultureInfo.InvariantCulture),
+                requestStatus = Constants.RequestStatus.Open,
+                timeOpened = DateTime.Now
+            };
+            var rmc = new RequestManagementController();
+            long rid1 = rmc.create(rc1);
+
             // Attempt to go to the Create Request Page
             _driver.Navigate().GoToUrl(CommonTestingMethods.getURL());
             _ctm.findAndClick(Constants.UIString.ItemIDs.CREATE_REQUEST,
@@ -162,9 +176,62 @@ namespace CAIRSTestProject.Integration {
 
             String rFName = "CrInt-" + Membership.GeneratePassword(10, 0);
             String rLName = "CrInt-" + Membership.GeneratePassword(10, 0);
-
+            String rEmail = "abcd@example.com";
+            String rPhone = "CrInt-" + Membership.GeneratePassword(10, 0);
+            String rExt = "CrInt-" + Membership.GeneratePassword(10, 0);
             String pFName = "CrInt-" + Membership.GeneratePassword(10, 0);
             String pLName = "CrInt-" + Membership.GeneratePassword(10, 0);
+            String pId = "CrInt-" + Membership.GeneratePassword(10, 0);
+            String pAge = "123";
+            String timeSpent = "1234";
+            String parentRequest = rid1.ToString();
+            String reference = "CrInt-" + Membership.GeneratePassword(10, 0);
+
+            _driver.FindElement(By.Id("requestorFirstName"))
+                   .SendKeys(rFName);
+            _driver.FindElement(By.Id("requestorLastName"))
+                   .SendKeys(rLName);
+            _driver.FindElement(By.Id("requestorEmail"))
+                   .SendKeys(rEmail);
+            _driver.FindElement(By.Id("requestorPhoneNum"))
+                   .SendKeys(rPhone);
+            _driver.FindElement(By.Id("requestorPhoneExt"))
+                   .SendKeys(rExt);
+            _driver.FindElement(By.Id("patientFName"))
+                   .SendKeys(pFName);
+            _driver.FindElement(By.Id("patientLName"))
+                   .SendKeys(pLName);
+            _driver.FindElement(By.Id("patientAgencyID"))
+                   .SendKeys(pId);
+            _driver.FindElement(By.Id("patientAge"))
+                   .SendKeys(pAge);
+            _driver.FindElement(By.ClassName("time-Spent"))
+                   .SendKeys(timeSpent);
+            _driver.FindElement(By.Id("parentRequestId"))
+                   .SendKeys(parentRequest);
+            _driver.FindElement(By.ClassName("reference"))
+                   .SendKeys(reference);
+
+            // Submit the Form
+            _driver.FindElement(By.Id("save_draft"));
+            StringAssert.Contains("/Request/Details", _driver.Url);
+
+            long newRid =
+                Convert.ToInt64(
+                    Regex.Match(_driver.Url, ".*\\/([0-9]+)").Groups[0]);
+
+            Request newReq =
+                _cdc.Requests.FirstOrDefault(r => r.RequestID == newRid);
+            if (newReq == null) {
+                Assert.Fail("Can't find newly-created Request.");
+            }
+
+            StringAssert.AreEqualIgnoringCase(rFName, newReq.RequestorFName);
+            StringAssert.AreEqualIgnoringCase(rLName, newReq.RequestorLName);
+            StringAssert.AreEqualIgnoringCase(rEmail, newReq.RequestorEmail);
+            StringAssert.AreEqualIgnoringCase(rPhone, newReq.RequestorPhone);
+            StringAssert.AreEqualIgnoringCase(rExt, newReq.RequestorPhoneExt);
+            StringAssert.AreEqualIgnoringCase(pFName, newReq.PatientFName);
         }
     }
 }
